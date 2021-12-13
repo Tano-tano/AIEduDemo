b813a288fd612adfe3f72c3b94318d1c7bb0c462
Tanotano-0718 <e1703@s.akashi.ac.jp>
2021-12-13 16:11:44 +0900

    2021/12/13

--- app/src/main/java/com/example/aiedudemo/main/DrawSelf.kt
+++ app/src/main/java/com/example/aiedudemo/main/DrawSelf.kt
@@ -1,24 +1,37 @@
 package com.example.aiedudemo.main
 
-//import com.divyanshu.draw.widget.DrawView
-
+import android.content.Context
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.aiedudemo.R
 import com.example.aiedudemo.view.DrawCanvas
 
 
 class DrawSelf : AppCompatActivity(){
-    private var drawView: View? = null
-
+    private var page = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.drawself)
+        val context: Context = applicationContext
 
-        drawView?.findViewById<View>(R.id.draw_view)
+        //５）Viewの取得⇒クリア処理
+        val drawView: DrawCanvas = findViewById(R.id.draw_view)
+        val resetBtn: Button = findViewById(R.id.resetBtn)
+        val saveBtn: Button = findViewById(R.id.saveBtn)
+        val picsNum: TextView = findViewById(R.id.picsnum)
 
-        DrawCanvas(this)
+        picsNum.text = page.toString()
 
+        resetBtn.setOnClickListener {
+            drawView.clearCanvas()
+        }
+
+        saveBtn.setOnClickListener {
+            page += 1
+            picsNum.text = page.toString()
+            drawView.clearCanvas()
+        }
     }
 }
--- app/src/main/java/com/example/aiedudemo/view/DrawCanvas.kt
+++ app/src/main/java/com/example/aiedudemo/view/DrawCanvas.kt
@@ -1,23 +1,53 @@
 package com.example.aiedudemo.view
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 
 
-class DrawCanvas(context: Context?) : View(context) {
-    private var paint: Paint? = null
-    private var pathList: ArrayList<Path> = ArrayList<Path>()
+class DrawCanvas(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
 
-    fun onTouch(v: View?, event: MotionEvent): Boolean {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {}
-            MotionEvent.ACTION_MOVE -> {}
-            MotionEvent.ACTION_UP -> {}
-            else -> {}
+    private var path : Path =Path()//線を引く、図形を描く、グラフィック描画
+    private var paint : Paint =Paint()//色とか太さ
+    private var drawX:Float= 0F
+    private var drawY:Float= 0F
+
+    //２）onDraw(描画の準備)
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        paint.color =Color.WHITE//色
+        paint.style=Paint.Style.STROKE//描画のスタイルを線にする
+        paint.strokeWidth = 70F//幅
+        canvas?.drawPath(path,paint)
+    }
+
+    //３）実際の描画（押した時、動かした時）
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        //タッチポジション（x座標、y座標）
+        drawX = event!!.x
+        drawY = event.y
+
+        when(event.action){
+            MotionEvent.ACTION_DOWN -> path.moveTo(drawX,drawY)
+            MotionEvent.ACTION_MOVE -> path.lineTo(drawX,drawY)
         }
+        //再描画を実行させる呪文
+        invalidate()
+
+        //return super.onTouchEvent(event)
         return true
+    }
+
+    //４）クリア処理（の関数を作る）
+    fun clearCanvas(){
+        path.reset()
+        invalidate()
     }
 }
