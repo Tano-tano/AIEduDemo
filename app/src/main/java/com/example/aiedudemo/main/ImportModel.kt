9e103055b3da67279a6bccf091ad8b349f106367
Tanotano-0718 <e1703@s.akashi.ac.jp>
2021-12-22 10:25:45 +0900

    モード実装完了

--- app/src/main/java/com/example/aiedudemo/main/ImportModel.kt
+++ app/src/main/java/com/example/aiedudemo/main/ImportModel.kt
@@ -1,45 +1,82 @@
 package com.example.aiedudemo.main
 
-import android.app.Activity
-import android.app.Instrumentation
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import com.divyanshu.draw.widget.DrawView
 import com.example.aiedudemo.R
 
 class ImportModel: AppCompatActivity() {
     var openFile:Button? = null
+    private var drawView: DrawView? = null
+    private var resetButton: Button? = null
+    private var inferButton: Button? = null
+    private var backButton: Button? = null
+    private var resultnum: TextView? = null
 
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.importmodel)
 
         openFile = findViewById(R.id.openFile)
+        drawView = findViewById(R.id.draw_view)
+        drawView?.setStrokeWidth(70.0f)
+        drawView?.setColor(Color.WHITE)
+        drawView?.setBackgroundColor(Color.BLACK)
+        resetButton = findViewById(R.id.resetBtn)
+        inferButton = findViewById(R.id.inferBtn)
+        backButton = findViewById(R.id.backBtn)
+        resultnum = findViewById(R.id.resultNum)
 
+        //自作モデルをインポートする
         openFile?.setOnClickListener {
-            openFile()
+            selectFile()
+        }
+
+        //描画処理
+        drawView?.setOnTouchListener{ _, event ->
+            drawView?.onTouchEvent(event)
+            true
+        }
+
+    }
+
+    private val launcher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        Log.d("registerForActivityResult(result)", result.toString())
+
+        if (result.resultCode != RESULT_OK) {
+            return@registerForActivityResult
+        } else {
+            try {
+                result.data?.data?.also { uri : Uri ->
+                    val inputStream = contentResolver?.openInputStream(uri)
+
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this, "エラーが発生しました", Toast.LENGTH_LONG).show()
+            }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == 111 && resultCode == RESULT_OK){
-            val selectedFile = data?.data
-        }
-    }
-
-    private fun openFile() {
+    private fun selectFile(){
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
             addCategory(Intent.CATEGORY_OPENABLE)
             type = "*/*"
-            action = Intent.ACTION_GET_CONTENT
         }
-        startActivityForResult(Intent.createChooser(intent, "Select a file"), 111)
+        launcher.launch(intent)
     }
+
 }
---
+++ app/src/main/java/com/example/aiedudemo/main/SelfClassifier.kt
@@ -0,0 +1,19 @@
+package com.example.aiedudemo.main
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.TaskCompletionSource
+import org.tensorflow.lite.Interpreter
+import java.io.IOException
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.channels.FileChannel
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class SelfClassifier(private val context: Context) {
+
+}
